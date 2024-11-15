6f9f8002c62b074b0f59c718b0c8f9b7aae292f0
bbonanno03 <brunof.bonanno@gmail.com>
2024-11-15 14:13:22 -0300

    sets and proyectos

--- 03_Strings.py
+++ 03_Strings.py
@@ -63,3 +63,19 @@ print(language.lower())
 print(language.lower().isupper())
 print(language.startswith("Py"))
 print("Py" == "py")  # No es lo mismo
+
+
+
+# División
+
+language_slice = language[1:3]
+print(language_slice)
+
+language_slice = language[1:]
+print(language_slice)
+
+language_slice = language[-2]
+print(language_slice)
+
+language_slice = language[0:6:2]
+print(language_slice)
